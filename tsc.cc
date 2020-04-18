646152b999e935bbf13235811f504dd07faffa29
ezrabartlett <ezrabartlett@tamu.edu>
2020-04-17 20:00:13 -0500

    ad

--- tsc.cc
+++ tsc.cc
@@ -169,7 +169,7 @@ IReply Client::processCommand(std::string& input)
         to_unfollow.set_follow(target_name);
         
         command_reply.grpc_status = stub_->Unfollow(&command_context, to_unfollow, &status);
-    } else if(strncmp(input_copy, "LIST", 4)==0){processTimeline()}
+    } else if(strncmp(input_copy, "LIST", 4)==0){processTimeline();}
     
     //std::cout << command_reply.grpc_status << "_Status";
     if(status.status()==""){
